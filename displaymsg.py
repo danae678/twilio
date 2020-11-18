e26fa257dea902602ea6cbc9b93700c9c401e19e
Danae Payne <Danae@Danaes-MacBook.fios-router.home>
2020-11-18 15:36:15 -0500

    comments

--- displaymsg.py
+++ displaymsg.py
@@ -4,6 +4,7 @@ from twilio.twiml.messaging_response import MessagingResponse
 from pyfirmata import Arduino, util, STRING_DATA
 
 """
+
 This program will display a message sent to the Twilio number on a 16x02 LCD 
 using an Ardunio UNO
 
@@ -12,27 +13,23 @@ app = Flask(__name__)
 
 board = Arduino('/dev/cu.usbmodem14101')
 
-
 @app.route("/sms", methods=['GET', 'POST'])
 def incoming_sms():
     
-    """Send a dynamic reply to an incoming text message"""
-    # Get the message the user sent our Twilio number
+    # get the message sent to the Twilio number
     body = request.values.get('Body', None)
 
-    # Start our TwiML response
+    # create a response
     resp = MessagingResponse()
-    resp.message("Message recieved!") #respond to incoming message
+    resp.message("Message recieved!")
     
-    #display message on lcd 
+    # display message on lcd 
     board.send_sysex(STRING_DATA, util.str_to_two_byte_iter(body)) 
 
     print("Message body: " + str(body))
     print("Displayed on LCD screen!")
 
-    return str(resp)
+    return str(resp) # return the response
  
-
-
 if __name__ == "__main__":
     app.run(debug=True)
